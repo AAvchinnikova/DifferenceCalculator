64217de746ee6e0226055853ab594c2e4b8325fc
AAvchinnikova <avchinnikova_a@mail.ru>
2024-05-06 10:12:37 +0200

    Step 3

--- app/src/main/java/hexlet/code/Compare.java
+++ app/src/main/java/hexlet/code/Compare.java
@@ -5,7 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
 
 public class Compare {
 
@@ -13,14 +14,35 @@ public class Compare {
         Path absolutOfPath1 = filePath1.toAbsolutePath().normalize();
         Path absolutOfPath2 = filePath2.toAbsolutePath().normalize();
         if (!Files.exists(absolutOfPath1) || !Files.exists(absolutOfPath2)) {
-            throw  new Exception("File does not exist");
+            throw new Exception("File does not exist");
         }
-        File file1 = new File(absolutOfPath1.toUri());
-        File file2 = new File(absolutOfPath2.toUri());
         ObjectMapper mapper = new ObjectMapper();
-        Map mapOfFile1 = mapper.readValue(file1, Map.class);
-        Map mapOfFile2 = mapper.readValue(file2, Map.class);
-        System.out.println(mapOfFile1);
-        System.out.println(mapOfFile2);
+        TreeMap<String, String> mapOfFile1 = mapper.readValue(new File(absolutOfPath1.toUri()), TreeMap.class);
+        TreeMap<String, String>  mapOfFile2 = mapper.readValue(new File(absolutOfPath2.toUri()), TreeMap.class);
+        var resultDif = new LinkedHashMap<>();
+        mapOfFile1.forEach((key1, value1) -> {
+            if (!mapOfFile2.containsKey(key1)) {
+                var keyDelete = "- " + key1;
+                resultDif.put(keyDelete, value1);
+            } else {
+                mapOfFile2.forEach((key2, value2) -> {
+                    if (key1.equals(key2) && value1.equals(value2)) {
+                        resultDif.put(key1, value1);
+                    } else if (key1.equals(key2) && !value1.equals(value2)) {
+                        var keyDelete = "- " + key1;
+                        resultDif.put(keyDelete, value1);
+                        var keyAdd = "+ " + key1;
+                        resultDif.put(keyAdd, value2);
+                    }
+                });
+            }
+        });
+        mapOfFile2.forEach((key2, value2) -> {
+        if (!mapOfFile1.containsKey(key2)) {
+                var keyAdd = "+ " + key2;
+                resultDif.put(keyAdd, value2);
+            }
+        });
+        System.out.println(resultDif);
     }
 }
