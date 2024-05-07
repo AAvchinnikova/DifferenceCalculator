6e7169642d75c29d6caa07064459f7bfe1e84f41
AAvchinnikova <avchinnikova_a@mail.ru>
2024-05-07 09:12:16 +0200

    Step 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
     application
     id("checkstyle")
     jacoco
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
@@ -21,6 +24,8 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
 }
 
 tasks.test {
