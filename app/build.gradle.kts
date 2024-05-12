5c0a66debcc013aacdb7db39faf521bdd3b58c19
AAvchinnikova <avchinnikova_a@mail.ru>
2024-05-12 10:17:16 +0200

    Step 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     application
     id("checkstyle")
@@ -29,7 +32,14 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-    finalizedBy (tasks.jacocoTestReport)
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
 
 tasks.jacocoTestReport {
